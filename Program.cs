86d064edce71169468b68cd2bf23b1d369039bed
Xyrna <mammadov.mahammad2002@gmail.com>
2022-10-31 12:36:29 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace middle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //metnnen reqemleri cixartsin
+
+            
+            string digits = "";
+
+            Console.Write("Enter any text: ");
+            string msg = Console.ReadLine();
+
+            
+
+            foreach (var i in msg)
+            {
+                if (char.IsDigit(i))
+                {
+                    digits = digits + i;
+                    
+                    
+                }
+
+            }
+
+            if (msg == "")
+            {
+                Console.WriteLine("Please enter any text!");
+            }
+
+            else if (digits.Length != 0)
+            {
+                Console.Write("The text has these digits: ");
+                foreach (var k in digits)
+                {
+
+                    Console.Write(k + " ");
+                }
+                Console.WriteLine();
+                Console.WriteLine("Number of digits in the text: " + digits.Length);
+            }
+
+            else
+            {
+                Console.WriteLine("The text has no digits!");
+            }
+            
+           
+
+        }
+    }
+}
